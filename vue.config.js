eafcc12082f7a869824cec61c041be14187a095a
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2023-07-21 20:01:05 +0200

    Init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
-const { defineConfig } = require("@vue/cli-service");
+"use strict";
+exports.__esModule = true;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+var defineConfig = require('@vue/cli-service').defineConfig;
+var path = require("path");
 module.exports = defineConfig({
-  transpileDependencies: true,
+    pwa: {
+        name: 'Pastolatarnia',
+        themeColor: '#00647D',
+        appleMobileWebAppCapable: 'yes',
+        workboxPluginMode: 'GenerateSW'
+    },
+    configureWebpack: {
+        devServer: {
+            historyApiFallback: true
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src/'),
+                '>>': path.resolve(__dirname, 'public/')
+            }
+        }
+    },
+    devServer: {
+        port: 2137,
+        host: ''
+    }
 });
